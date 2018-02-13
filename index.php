cca6ade92b34bebd77bada5172af3d715dc9d062
hasinajedidia <36400308+hasinajedidia@users.noreply.github.com>
2018-02-13 09:39:58 +0100

    index.php
    test

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
+<?php
+echo "bonjour";
+/*
+//page access token
+$access_token = "EAAXQf6GIyZCcBADDCBkoW9WKygepyKZCO4XExwHOPtZCvlsRVrxiNIG89Y6DcWaaFHBYrtUNxOKsc9wF0CWGngrmECcaE4n493SUD6n6zhBATK4ZBHb5PaL08nM91krn5Cc6fiazZCNnjYBvl2VsV2lMVEaLphC1MZC207GxltpQZDZD";
+//webhook verification token
+$verify_token = "msa-bot-2018";
+$hub_verify_token = null;
 
+if(isset($_REQUEST['hub_challenge'])){
+    $challenge = $_REQUEST['hub_challenge'];
+    $hub_verify_token = $_REQUEST['hub_verify_token'];
+} 
+
+if($hub_verify_token == $verify_token){
+    echo $challenge;
+}
+
+//sending and receiving message
+$input = json_decode(file_get_contents('php://input'), true);
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
+
+$message_to_replay = '';
+*/
+?>
